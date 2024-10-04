7b6490b071dbe7a10e2be8de637f11cab5435697
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-10-04 23:37:03 +0200

    x

---
+++ functions/AddQuestionsQuiz/index.js
@@ -0,0 +1,42 @@
+const middy = require("@middy/core");
+const { validateToken } = require("../../middleware/auth");
+const { sendResponse, sendError } = require("../../responses/index");
+const { fetchQuiz, deleteQuiz } = require("../../services/quizService");
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
+
+const handler = middy()
+  .use(validateToken)
+  .handler(async (event) => {
+    try {
+      const username = event.username;
+
+      const { quizId, questions } = JSON.parse(event.body);
+
+      const quiz = await fetchQuiz(quizId, process.env.TABLE_NAME_QUIZ);
+      if (!quiz) {
+        return sendError(400, "No quiz w that id exits in db.");
+      }
+
+      if (username !== quiz.username) {
+        return sendError(
+          400,
+          "Current user didnt create this quiz and cannot delete it."
+        );
+      }
+
+      const updatedQuestions = questions.map((question) => ({
+        ...question,
+        questionId: uuidv4(),
+      }));
+
+      //add questions
+
+      return sendResponse(200, updatedQuestions);
+    } catch (error) {
+      console.error("Error occurred:", error);
+      return sendError(500, "Internal Server Error");
+    }
+  });
+
+module.exports = { handler };
