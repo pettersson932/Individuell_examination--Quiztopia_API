6f758ecdd0996f39b570c49f837f46d96ac06b6e
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-10-04 22:14:44 +0200

    x

---
+++ functions/FetchQuiz/index.js
@@ -0,0 +1,29 @@
+const { fetchQuiz } = require("../../services/quizService");
+const { sendResponse, sendError } = require("../../responses/index");
+
+exports.handler = async (event) => {
+  const { quizId } = JSON.parse(event.body);
+
+  try {
+    const quiz = await fetchQuiz(quizId, process.env.TABLE_NAME_QUIZ);
+    if (!quiz) {
+      return sendResponse(400, "quiz not found.");
+    }
+
+    const response = {
+      name: quiz.name,
+      questions: quiz.questions.map((question) => question.question),
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    console.error("Error fetching quiz:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error fetching quiz" }),
+    };
+  }
+};
